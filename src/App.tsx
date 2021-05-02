66ac0887f5dc1f7960cc780bc6398c2c65c5f0f7
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-01 21:16:17 -0300

    feat: implement Routes on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { ThemeProvider } from '@emotion/react'
 
+import Routes from './Routes'
 import theme from './theme'
 
-const App = () => <ThemeProvider theme={theme}></ThemeProvider>
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Routes />
+  </ThemeProvider>
+)
 
 export default App
