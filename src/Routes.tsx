8c8979be8e35fb62be6e9867f0e0e53e46e3bf4d
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-01 21:24:20 -0300

    feat: create Home page component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,11 +6,15 @@ import {
   Switch,
 } from 'react-router-dom'
 
+import Home from './pages/home'
+
 const Routes = () => (
   <Router>
     <Switch>
       <Route path="/builder"></Route>
-      <Route path="/home"></Route>
+      <Route path="/home">
+        <Home />
+      </Route>
       <Route path="/">
         <Redirect to="/home" />
       </Route>
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return <Link to="/builder">Click here to build your Workflow!</Link>
+}
+
+export default Home
