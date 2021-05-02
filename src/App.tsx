6b5043d4d71e37c90ac13f43518d139b513a508c
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-01 21:05:08 -0300

    chore: cleanup files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { ThemeProvider } from '@emotion/react'
 
 import theme from './theme'
-import './App.css'
 
 const App = () => <ThemeProvider theme={theme}></ThemeProvider>
 
