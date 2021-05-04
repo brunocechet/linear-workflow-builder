84360ed8b1d9d1e4a5f11a4889f176f05dbfb9ce
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-04 00:15:26 -0300

    feat: Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { ReactComponent as HeaderImage } from './images/undraw_online_connection_6778.svg'
+
 const Home = () => {
-  return <Link to="/builder">Click here to build your Workflow!</Link>
+  return (
+    <>
+      <HeaderImage height={300} />
+      <h1>Welcome to Workflow Builder!</h1>
+      <Link to="/builder">Click here to build your Workflow!</Link>
+    </>
+  )
 }
 
 export default Home
