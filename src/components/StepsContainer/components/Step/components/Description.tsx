a0b8057b20576837f12051d7468032aa2730213e
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-08 12:56:19 -0300

    refactor: preffer interfaces

--- src/components/StepsContainer/components/Step/components/Description.tsx
+++ src/components/StepsContainer/components/Step/components/Description.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { space, SpaceProps, typography, TypographyProps } from 'styled-system'
-import styled from '@emotion/styled'
+import React from "react";
+import { space, SpaceProps, typography, TypographyProps } from "styled-system";
+import styled from "@emotion/styled";
 
-type DescriptionBaseProps = TypographyProps &
-  SpaceProps &
-  React.HTMLAttributes<HTMLParagraphElement>
+interface IDescriptionBase
+  extends TypographyProps,
+    SpaceProps,
+    React.HTMLAttributes<HTMLParagraphElement> {}
 
-const DescriptionBase = styled.p<DescriptionBaseProps>`
+const DescriptionBase = styled.p<IDescriptionBase>`
   ${space};
   ${typography};
-`
+`;
 
 const Description: React.FC = ({ children }) => (
   <DescriptionBase fontSize={0} p={0} my={0}>
     {children}
   </DescriptionBase>
-)
+);
 
-export default Description
+export default Description;
