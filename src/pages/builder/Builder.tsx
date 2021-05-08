1b8496b0b243fd61c406d6f855369a5c46a1518a
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-08 12:57:11 -0300

    feat: Builder page with drag and drop functionality

--- src/pages/builder/Builder.tsx
+++ src/pages/builder/Builder.tsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { nanoid } from "nanoid";
 
-import StepsContainer from '../../components/StepsContainer'
+import StepsContainer from "../../components/StepsContainer";
 
 const TEMP_MOCK = Array(6)
   .fill({})
-  .map((_, index) => ({
-    title: `Title for ${index}`,
-    description: `Description for ${index}`,
-  }))
+  .map((_, index) => {
+    const id = nanoid();
+    return {
+      title: `Title for ${id}`,
+      description: `Description for ${id}`,
+      id,
+      index,
+    };
+  });
 
 const Builder: React.FC = () => {
-  return <StepsContainer steps={TEMP_MOCK} />
-}
+  const [steps, setSteps] = useState(TEMP_MOCK);
 
-export default Builder
+  const handleOnDragEnd = (result: DropResult) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    // same position
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const items = Array.from(steps);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setSteps(items);
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <StepsContainer steps={steps} />
+    </DragDropContext>
+  );
+};
+
+export default Builder;
