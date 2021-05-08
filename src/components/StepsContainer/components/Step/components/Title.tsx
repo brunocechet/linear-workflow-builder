82dd4338ada2e3f94fc5dafabf90faff5a916273
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-08 12:56:23 -0300

    refactor: preffer interfaces

--- src/components/StepsContainer/components/Step/components/Title.tsx
+++ src/components/StepsContainer/components/Step/components/Title.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { typography, TypographyProps, space, SpaceProps } from 'styled-system'
-import styled from '@emotion/styled'
+import React from "react";
+import { typography, TypographyProps, space, SpaceProps } from "styled-system";
+import styled from "@emotion/styled";
 
-type TitleBaseProps = TypographyProps &
-  SpaceProps &
-  React.HTMLAttributes<HTMLParagraphElement>
+interface ITitleBase
+  extends TypographyProps,
+    SpaceProps,
+    React.HTMLAttributes<HTMLParagraphElement> {}
 
-const TitleBase = styled.p<TitleBaseProps>`
+const TitleBase = styled.p<ITitleBase>`
   ${space}
   ${typography};
-`
+`;
 
 const Title: React.FC = ({ children }) => (
   <TitleBase fontSize={1} p={0} m={0} fontWeight="semibold">
     {children}
   </TitleBase>
-)
+);
 
-export default Title
+export default Title;
