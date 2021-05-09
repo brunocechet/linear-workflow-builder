59f235853f66d8e8ce208b35a7ad3fa80fe4a53d
Bruno Kuhlmann Cechet <brunocechet@gmail.com>
2021-05-09 15:54:41 -0300

    test: config mock for react-beautiful-dnd

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,32 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+interface Props {
+  children: Function;
+}
+
+
+jest.mock("react-beautiful-dnd", () => ({
+  Droppable: ({ children }: Props) =>
+    children(
+      {
+        draggableProps: {
+          style: {},
+        },
+        innerRef: jest.fn(),
+      },
+      {}
+    ),
+  Draggable: ({ children }: Props) =>
+    children(
+      {
+        draggableProps: {
+          style: {},
+        },
+        innerRef: jest.fn(),
+      },
+      {}
+    ),
+  DragDropContext: ({ children }: Props) => children,
+}));
